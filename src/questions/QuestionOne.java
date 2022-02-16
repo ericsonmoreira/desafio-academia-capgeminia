978871a83f7264e4c7af0464ddcbec1e15083a43
ericsonmoreira <moreira.ericson@gmail.com>
2022-02-16 09:35:34 -0300

    add QuestionOne

---
+++ src/questions/QuestionOne.java
@@ -0,0 +1,39 @@
+package questions;
+
+/**
+ * 1. Escreva um algoritmo que mostre na tela uma escada de tamanho n utilizando o caractere * e espaços. A base e
+ * altura da escada devem ser iguais ao valor de n. A última linha não deve conter nenhum espaço.
+ *
+ * Exemplo:
+ * Entrada:
+ * n = 6
+ *
+ * Saída:
+ *      *
+ *     **
+ *    ***
+ *   ****
+ *  *****
+ * ******
+ */
+
+public class QuestionOne {
+
+    public static String generateLadder(int n) {
+        String result = "";
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 0; j < n - i; j++) result += " ";
+
+            for (int j = 0; j < i; j++) result += "*";
+
+            result += "\n";
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(generateLadder(5));
+    }
+}
